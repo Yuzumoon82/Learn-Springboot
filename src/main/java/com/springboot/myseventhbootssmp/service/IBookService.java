9a03e7495b51749a99d4a19f82b72d088ae56915
Yuzumoon82 <2920253337@qq.com>
2026-09-08 21:01:23 +0800

    表现层及数据一致性，axios发送异步请求

--- src/main/java/com/springboot/myseventhbootssmp/service/IBookService.java
+++ src/main/java/com/springboot/myseventhbootssmp/service/IBookService.java
@@ -1,7 +1,11 @@
 package com.springboot.myseventhbootssmp.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.spring.service.IService;
 import com.springboot.myseventhbootssmp.domain.Book;
 
 public interface IBookService extends IService<Book> {
+    boolean delete(Integer id);
+    boolean modify(Book book);
+    IPage<Book> getPage(int currentPage, int pageSize);
 }
